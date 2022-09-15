c64867a05c42bcc7eae895ddbd5217a025415b04
niamhbrockbank <niamh.brockbank@gmail.com>
2022-09-15 20:28:36 +0100

    Background styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,14 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "holderjs";
-import Task from "./components/Task";
 import "./App.css";
+import MatrixBackground from "./components/MatrixBackground";
+import Title from "./components/Title";
 
 function App(): JSX.Element {
   return (
     <>
-      <Task />
-      <Task />
-      <Task />
-      <Task />
-      <div className="grid_box">
-        <div className="grid">Do first</div>
-        <div className="grid">Schedule</div>
-        <div className="grid">Delegate</div>
-        <div className="grid">Forget about it</div>
-      </div>
+      <Title />
+      <MatrixBackground />
     </>
   );
 }
---
+++ src/components/MatrixBackground.tsx
@@ -0,0 +1,10 @@
+export default function MatrixBackground():JSX.Element{
+    return (
+        <div className="grid_box">
+            <div className="grid">Do first</div>
+            <div className="grid">Schedule</div>
+            <div className="grid">Delegate</div>
+            <div className="grid">Forget about it</div>
+        </div>
+    )
+}

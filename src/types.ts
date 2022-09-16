a8024c92d5597fa62a1edf8958d00da79828e2d0
niamhbrockbank <niamh.brockbank@gmail.com>
2022-09-16 09:28:02 +0100

    Remove useStateManager

--- src/types.ts
+++
@@ -1,4 +0,0 @@
-export interface IUseStateManager{
-    forgetAboutItItems : string[]; 
-    setForgetAboutItItems : React.Dispatch<React.SetStateAction<string[]>>;
-}
--- src/utils/useStateManager.tsx
+++
@@ -1,8 +0,0 @@
-import { useState } from "react";
-import { IUseStateManager } from "../types";
-
-export const useStateManager = ():IUseStateManager => {
-  const [forgetAboutItItems, setForgetAboutItItems] = useState<string[]>([]);
-
-  return { forgetAboutItItems, setForgetAboutItItems };
-};

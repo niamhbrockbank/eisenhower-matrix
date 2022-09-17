f21314775020f214ecd80e04c1e4f857cec30564
niamhbrockbank <niamh.brockbank@gmail.com>
2022-09-17 10:02:00 +0100

    Format with prettier

--- src/components/MatrixBackground/ForgetAboutIt.tsx
+++ src/components/MatrixBackground/ForgetAboutIt.tsx
@@ -9,18 +9,14 @@ export default function ForgetAboutIt({
   forgetAboutItItemsManager,
 }: ForgetAboutItProps): JSX.Element {
   const [forgetAboutItItems] = forgetAboutItItemsManager;
-  console.log(forgetAboutItItems)
+  console.log(forgetAboutItItems);
 
-  const tempItems = [
-    { id: "1", note: "do something" },
-    { id: "2", note: "do something else" },
-    { id: "3", note: "hi" },
-    { id: "4", note: "working?" },
-  ];
+  // const tempItems = [
+  //   { id: "1", note: "do something" },
+  //   { id: "2", note: "do something else" },
+  //   { id: "3", note: "hi" },
+  //   { id: "4", note: "working?" },
+  // ];
 
-  return (
-    <div className="grid">
-      Forget about it
-    </div>
-  );
+  return <div className="grid">Forget about it</div>;
 }

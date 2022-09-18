28c2381b2e53606fa0c1935c685c69979a0472b1
niamhbrockbank <niamh.brockbank@gmail.com>
2022-09-18 17:55:26 +0100

    Add mouse down event handler button

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,32 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "holderjs";
-import { useState } from "react";
+import { useState, MouseEvent } from "react";
 import "./App.css";
 import MatrixBackground from "./components/MatrixBackground/MatrixBackground";
 import NewNote from "./components/NewNote/NewNote";
-import SketchP5Trial from "./components/SketchP5Trial";
+// import SketchP5Trial from "./components/SketchP5Trial";
 import Title from "./components/Title";
 
 function App(): JSX.Element {
   const forgetAboutItItemsManager = useState<string[]>([]);
+  const [state, setState] = useState({message : 'Mouse Event'})
+
+  const handleEvent= (event : MouseEvent) => {
+    if (event.type === 'mousedown'){
+      setState({message : "Mouse Down"})
+    } else {
+      setState({message : "Mouse Up"})
+    }
+  } 
 
   return (
     <>
       <Title />
       <MatrixBackground forgetAboutItItemsManager={forgetAboutItItemsManager} />
       <NewNote forgetAboutItItemsManager={forgetAboutItItemsManager} />
-      <SketchP5Trial />
+      {/* <SketchP5Trial /> */}
+      <div>{state.message}</div>
+      <button onMouseDown={handleEvent} onMouseUp={handleEvent}>Change image</button>
     </>
   );
 }

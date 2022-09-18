f88325d6d49f4883bb9a0380e1c3201fab10d963
niamhbrockbank <niamh.brockbank@gmail.com>
2022-09-18 22:32:12 +0100

    Format with prettier

--- src/App.tsx
+++ src/App.tsx
@@ -6,48 +6,53 @@ import MatrixBackground from "./components/MatrixBackground/MatrixBackground";
 import NewNote from "./components/NewNote/NewNote";
 import Title from "./components/Title";
 
-interface Note{
-  id : number,
-  note_body : string,
-  position : {
-    x :number,
-    y : number
-  }
+interface Note {
+  id: number;
+  note_body: string;
+  position: {
+    x: number;
+    y: number;
+  };
 }
 
 function App(): JSX.Element {
   const forgetAboutItItemsManager = useState<string[]>([]);
-  
-  const tempItems : Note[] = [
-    { id: 1, note_body: "do something", position : {x : 20, y : 50} },
-    { id: 2, note_body: "do something else", position : {x : 20, y : 50} },
-    { id: 3, note_body: "hi" , position : {x : 30, y : 75}},
-    { id: 4, note_body: "working?", position : {x : 10, y : 50} },
+
+  const tempItems: Note[] = [
+    { id: 1, note_body: "do something", position: { x: 20, y: 50 } },
+    { id: 2, note_body: "do something else", position: { x: 20, y: 50 } },
+    { id: 3, note_body: "hi", position: { x: 30, y: 75 } },
+    { id: 4, note_body: "working?", position: { x: 10, y: 50 } },
   ];
 
-  const [tempItemsState, setTempItemsState] = useState(tempItems)
+  const [tempItemsState, setTempItemsState] = useState(tempItems);
 
-  const handleDragEnd = (e : MouseEvent, draggedId : number) => {
-    const copyTempItems = [...tempItemsState]
-    const draggedIndex = copyTempItems.findIndex(note => note.id === draggedId)
-    copyTempItems[draggedIndex].position = {x : e.clientX, y: e.clientY}
-    setTempItemsState(copyTempItems)
-  }
+  const handleDragEnd = (e: MouseEvent, draggedId: number) => {
+    const copyTempItems = [...tempItemsState];
+    const draggedIndex = copyTempItems.findIndex(
+      (note) => note.id === draggedId
+    );
+    copyTempItems[draggedIndex].position = { x: e.clientX, y: e.clientY };
+    setTempItemsState(copyTempItems);
+  };
 
-  const convertNotetoElement = (note : Note) : JSX.Element => {
+  const convertNotetoElement = (note: Note): JSX.Element => {
     return (
-      <div 
-        key={note.id} 
-        className='draggable' 
-        draggable='true'
+      <div
+        key={note.id}
+        className="draggable"
+        draggable="true"
         onDragEnd={(e) => handleDragEnd(e, note.id)}
-        style={{position : 'absolute',
-                left : `${note.position.x}px`,
-                top : `${note.position.y}px`}}>
-          {note.note_body}
+        style={{
+          position: "absolute",
+          left: `${note.position.x}px`,
+          top: `${note.position.y}px`,
+        }}
+      >
+        {note.note_body}
       </div>
-      )
-  }
+    );
+  };
 
   return (
     <>
@@ -55,7 +60,9 @@ function App(): JSX.Element {
       <MatrixBackground forgetAboutItItemsManager={forgetAboutItItemsManager} />
       <NewNote forgetAboutItItemsManager={forgetAboutItItemsManager} />
 
-      <div id='easy_test' className='draggable' draggable='true'>this will be draggable</div>
+      <div id="easy_test" className="draggable" draggable="true">
+        this will be draggable
+      </div>
       {tempItemsState.map(convertNotetoElement)}
     </>
   );

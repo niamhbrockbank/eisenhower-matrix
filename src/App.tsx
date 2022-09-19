23e891bfa5e716bd3067d0792a35244fa3fe80e8
niamhbrockbank <niamh.brockbank@gmail.com>
2022-09-19 17:20:33 +0100

    Change initial position of notes

--- src/App.tsx
+++ src/App.tsx
@@ -12,10 +12,10 @@ function App(): JSX.Element {
   const [offset, setOffset] = useState<Offset>({ xOffset: 0, yOffset: 0 });
 
   const fillerNotes: Note[] = [
-    { id: 1, note_body: "do something", position: { x: 20, y: 50 } },
-    { id: 2, note_body: "do something else", position: { x: 20, y: 50 } },
-    { id: 3, note_body: "hi", position: { x: 30, y: 75 } },
-    { id: 4, note_body: "working?", position: { x: 10, y: 50 } },
+    { id: 1, note_body: "do something", position: { x: 115, y: 630 } },
+    { id: 2, note_body: "do something else", position: { x: 650, y: 170 } },
+    { id: 3, note_body: "hi", position: { x: 250, y: 230 } },
+    { id: 4, note_body: "working?", position: { x: 475, y: 200 } },
   ];
 
   const [notesArr, setNotesArr] = useState(fillerNotes);

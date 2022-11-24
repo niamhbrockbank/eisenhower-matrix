f082c09bc334a98db9702881b513f3716c7690a6
niamhbrockbank <niamh.brockbank@gmail.com>
2022-11-24 14:17:57 +0000

    Update backend address for delete button

--- src/utils/deleteNote.ts
+++ src/utils/deleteNote.ts
@@ -4,7 +4,7 @@ export default async function deleteNote(
   note_id: number,
   getNotes: () => Promise<void>
 ): Promise<void> {
-  await axios.delete("https://priorities-measure.herokuapp.com/notes", {
+  await axios.delete("https://priorities-measure.onrender.com/notes", {
     data: { note_id: note_id },
   });
   await getNotes();
